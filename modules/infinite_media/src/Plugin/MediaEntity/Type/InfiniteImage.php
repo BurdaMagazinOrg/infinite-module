6e1970d1b2a8dbf5eca725bd36b2b8f1b3b8fa8e
Lucio Waßill <lucio@undpaul.de>
2016-01-25 16:58:07 +0100

    Fix infinite media after drupal update
    When updating to the latest Thunder Version, Drupal will be updated to 8.0.2.
    
    In this version, usage of EntityManager is deprecated and uploading images will fail when trying to create a new entity.
    
    This commit also fixes an issue having two plugins of this image bundle.
    
    Signed-off-by: Lucio Waßill <lucio@undpaul.de>

--- modules/infinite_media/src/Plugin/MediaEntity/Type/InfiniteImage.php
+++ modules/infinite_media/src/Plugin/MediaEntity/Type/InfiniteImage.php
@@ -1,22 +1,18 @@
 <?php
-
 /**
  * Contains \Drupal\infinite_media\Plugin\MediaEntity\Type\InfiniteImage.
  */
-
 namespace Drupal\infinite_media\Plugin\MediaEntity\Type;
-
-use Drupal\Component\Plugin\PluginBase;
 use Drupal\Core\Config\Config;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Image\ImageFactory;
 use Drupal\media_entity\MediaBundleInterface;
 use Drupal\media_entity\MediaInterface;
 use Drupal\media_entity\MediaTypeBase;
-use Drupal\media_entity\MediaTypeInterface;
-use Drupal\file\FileInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
+use Drupal\Core\Form\FormStateInterface;
 /**
  * Provides generic media type.
  *
@@ -27,47 +23,75 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class InfiniteImage extends MediaTypeBase {
-
+  /**
+   * Constructs a new class instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager service.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   Entity field manager service.
+   * @param \Drupal\Core\Image\ImageFactory $image_factory
+   *   The image factory.
+   * @param \Drupal\Core\Config\Config $config
+   *   Media entity config object.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager, ImageFactory $image_factory, Config $config) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $entity_field_manager, $config);
+    $this->imageFactory = $image_factory;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('entity_field.manager'),
+      $container->get('image.factory'),
+      $container->get('config.factory')->get('media_entity.settings')
+    );
+  }
   /**
    * {@inheritdoc}
    */
   public function providedFields() {
     return array();
   }
-
   /**
    * {@inheritdoc}
    */
   public function getField(MediaInterface$media, $name) {
     return FALSE;
   }
-
   /**
    * {@inheritdoc}
    */
   public function settingsForm(MediaBundleInterface $bundle) {
     return array();
   }
-
   /**
    * {@inheritdoc}
    */
   public function validate(MediaInterface $media) { }
-
   /**
    * {@inheritdoc}
    */
   public function thumbnail(MediaInterface $media) {
     $source_field = 'field_image';
-
     /** @var \Drupal\file\FileInterface $file */
-    $file = $this->entityManager->getStorage('file')->load($media->{$source_field}->target_id);
-
+    $file = $this->entityTypeManager->getStorage('file')
+      ->load($media->{$source_field}->target_id);
     if (!$file) {
-      return $this->config->get('icon_base') . '/generic.png';
+      return $this->config->get('icon_base') . '/image.png';
     }
-
     return $file->getFileUri();
   }
-
 }
