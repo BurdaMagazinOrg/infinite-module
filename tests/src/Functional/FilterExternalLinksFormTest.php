deb35513d56ae926a7dfadc51f201c1b099171e8
Florian Weber <florian.weber@ueberbit.de>
2016-07-15 16:50:18 +0200

    Remove instyle.de default config, ignore empty lines and trim all values

---
+++ tests/src/Functional/FilterExternalLinksFormTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\Tests\infinite_base\Functional;
+
+use Drupal\Core\Url;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test infinite_base admin interface.
+ *
+ * @group infinite_base
+ */
+class FilterExternalLinksFormTest extends BrowserTestBase {
+
+  protected static $modules = ['infinite_base'];
+
+  protected function setUp() {
+    parent::setUp();
+    $this->drupalLogin($this->drupalCreateUser(['administer filters']));
+  }
+
+  public function testForm() {
+    $this->drupalGet(Url::fromRoute('infinite_base.filter_external_links'));
+    $page = $this->getSession()->getPage();
+
+    $domains = [
+      'example.com',
+      '',
+      ' ',
+      'foo.com ',
+      ' bar.de'
+    ];
+    $page->fillField('internal_domains', implode("\r\n", $domains));
+    $page->pressButton('Save configuration');
+
+    $domains = [
+      'example.com',
+      'foo.com',
+      'bar.de',
+    ];
+    $this->assertSame($domains, \Drupal::config('infinite_base.filter_external_links')->get('internal_domains'));
+  }
+
+}
