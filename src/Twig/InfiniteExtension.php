00fc1259263ac039a1e102d8533c1cfaae29eaae
Stefan Freudenberg <stefan@agaric.com>
2017-02-22 19:31:44 +0100

    Add class and method documentation

--- src/Twig/InfiniteExtension.php
+++ src/Twig/InfiniteExtension.php
@@ -1,23 +1,44 @@
 <?php
 
+/**
+ * @file
+ * Provides Drupal\infinite_base\Twig\InfiniteExtension
+ */
+
 namespace Drupal\infinite_base\Twig;
 
 use Twig_Extension;
 use Twig_SimpleFilter;
 use function render;
 
+/**
+ * A Twig extension for infinite themes.
+ */
 class InfiniteExtension extends Twig_Extension {
 
+  /**
+   * {@inheritdoc}
+   */
   public function getFilters() {
     return array(
       new Twig_SimpleFilter('plain_text', array($this, 'plainText')),
     );
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getName() {
     return 'infinite_base.twig_extension';
   }
 
+  /**
+   * Returns a plain text representation of the given value.
+   *
+   * @param mixed $value
+   *
+   * @return string
+   */
   public static function plainText($value) {
     $element = render($value);
     $element = strip_tags($element);
