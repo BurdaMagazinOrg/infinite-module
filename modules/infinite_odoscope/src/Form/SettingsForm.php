15f588c181630f85c6cee44015ed58eb71a3af51
Gerhard Killesreiter <gerhard@tag1consulting.com>
2017-08-29 13:29:28 +0200

    add a settings form

---
+++ modules/infinite_odoscope/src/Form/SettingsForm.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\infinite_odoscope\Form\SettingsForm.
+ */
+
+namespace Drupal\infinite_odoscope\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class SettingsForm.
+ *
+ * @package Drupal\infinite_odoscope\Form
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'infinite_odoscope.config'
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'infinite_odoscope_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('infinite_odoscope.config');
+
+    $form['odoscope_user'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Odoscope user'),
+      '#description' => $this->t('Used for uploading CSV updates.'),
+      '#maxlength' => 128,
+      '#required' => TRUE,
+      '#default_value' => $config->get('odoscope_user'),
+    );
+    $form['odoscope_pass'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Odoscope password'),
+      '#description' => $this->t('Used for uploading CSV updates.'),
+      '#maxlength' => 128,
+      '#required' => TRUE,
+      '#default_value' => $config->get('odoscope_pass'),
+    );
+    $form['odoscope_url'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Odoscope URL'),
+      '#description' => $this->t('Used for uploading CSV updates.'),
+      '#maxlength' => 128,
+      '#required' => TRUE,
+      '#default_value' => $config->get('odoscope_url'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('infinite_odoscope.config')
+      ->set('odoscope_user', $form_state->getValue('odoscope_user'))
+      ->save();
+
+    $this->config('infinite_odoscope.config')
+      ->set('odoscope_pass', $form_state->getValue('odoscope_pass'))
+      ->save();
+
+    $this->config('infinite_odoscope.config')
+      ->set('odoscope_url', $form_state->getValue('odoscope_url'))
+      ->save();
+  }
+}
