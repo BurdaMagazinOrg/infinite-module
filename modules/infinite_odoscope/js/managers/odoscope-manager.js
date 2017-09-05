9644d8225b379b11815477125a312e3cf1bc2250
Nico Davis <nicod@gmx.net>
2017-09-05 13:52:53 +0200

    update infinite odoscope

---
+++ modules/infinite_odoscope/js/managers/odoscope-manager.js
@@ -0,0 +1,121 @@
+(function ($, Drupal, drupalSettings, Backbone, BurdaInfinite) {
+
+  "use strict";
+
+  BurdaInfinite.managers.OdoscopeManager = Backbone.Model.extend({
+    instance: null,
+    defaults: {
+      active: false,
+      type: "",
+      group: "",
+      basehost: window.location.hostname.split(".")[0]
+    },
+    initialize: function () {
+      this.createModels();
+    },
+    activate: function (pOdoscopeAttr) {
+      this.set(pOdoscopeAttr);
+      this.set('active', true);
+
+      // this.setGroupCookie(this.get('group'));
+      console.log("%codoscopeManager | activate", "color: green; font-weight: bold;", pOdoscopeAttr, this);
+
+      TrackingManager.trackEvent({
+        event: AppConfig.gtmEventName,
+        category: OdoscopeManager.GTM_EVENT_CATEGORY,
+        action: 'pageview',
+        label: this.get('group')
+      });
+    },
+    createModels: function () {
+
+      if (typeof OdoscopeArticleModel != "undefined") {
+        this.set('articleModel', new OdoscopeArticleModel());
+        console.log("%codoscopeManager | createModels | OdoscopeArticleModel", "color: green; font-weight: bold;");
+      }
+
+    },
+    isOdoscopeArticleGroup: function () {
+      return this.get('group') == OdoscopeManager.GROUP_ODOSCOPE && this.get('type') == OdoscopeManager.ODOSCOPE_TYPE_ARTICLE;
+    },
+    isOdoscopeGroup: function () {
+      return this.getGroup() == OdoscopeManager.GROUP_ODOSCOPE;
+    },
+    getGroup: function () {
+      return this.get('group');
+    },
+    getType: function () {
+      return this.get('type');
+    },
+    getTrackingObject: function () {
+      if (this.getGroup() == undefined) {
+        return null;
+
+      }
+      return {group: this.getGroup(), type: this.getType()};
+    }
+  }, {
+    ODOSCOPE_TYPE_ARTICLE: 'articlePage',
+    ODOSCOPE_TYPE_FEED: 'feedPage',
+    GROUP_ODOSCOPE: 'odoscope',
+    GROUP_CONTROL: 'control',
+    GTM_EVENT_CATEGORY: 'odoscope',
+    getInstance: function () {
+      if (this.instance == null) {
+        this.instance = new OdoscopeManager();
+        this.instance.constructor = null;
+      }
+
+      return this.instance;
+    }
+  });
+
+  window.OdoscopeManager = window.OdoscopeManager || BurdaInfinite.managers.OdoscopeManager;
+
+  window.oscSplitTestCallback = function (pType, pGroup) {
+
+    console.log("%cwindow | oscSplitTestCallback", "color: green; font-weight: bold;", pType, pGroup);
+
+    var tmpOdoscopeAttr = {type: pType, group: pGroup};
+    OdoscopeManager.getInstance().activate(tmpOdoscopeAttr);
+  };
+
+  window.oscInitializeArticlePageRendering = function (pDecisionFunction, pConfig, p1, p2) {
+    console.log("%codoscope | articlePageRendering", "color: green; font-weight: bold;", pConfig, p1, p2);
+
+    pDecisionFunction({}, function (pArticles, p1, p2) {
+      console.log("%codoscope | articlePageRendering  | decision", "color: green; font-weight: bold;", OdoscopeManager.getInstance().getGroup(), pArticles, p1, p2);
+
+      // if (OdoscopeManager.getInstance().isOdoscopeGroup()) {
+      OdoscopeManager.getInstance().get('articleModel').create(pArticles);
+
+    }, function (pError) {
+      console.log("%codoscope | articlePageRendering | error", "color: red; font-weight: bold;", pError);
+      // error callback
+    });
+  };
+
+
+  window.oscTeaserElementReplaced = function (pElement) {
+    // apply social icon code and timeago
+
+    console.log(">>> window.oscTeaserElementReplaced", pElement);
+  };
+
+  window.oscInfiniteBlockViewUpdated = function (pElement) {
+    Drupal.behaviors.blazy.attach(pElement);
+    jQuery(pElement).data('infiniteModel').refresh();
+    // update waypoints
+    window.Waypoint.refreshAll();
+    console.log(">>> window.oscInfiniteBlockViewUpdated", pElement);
+
+    if (TrackingManager != undefined) {
+      TrackingManager.trackEvent({
+        event: AppConfig.gtmEventName,
+        category: OdoscopeManager.GTM_EVENT_CATEGORY,
+        action: 'feedInfiniteBlockRendered'
+      });
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings, Backbone, BurdaInfinite);
---
+++ modules/infinite_odoscope/js/models/odoscope-article-model.js
@@ -0,0 +1,50 @@
+(function ($, Drupal, drupalSettings, Backbone, BurdaInfinite) {
+
+  "use strict";
+
+  window.OdoscopeArticleModel = BurdaInfinite.models.base.BaseModel.extend({
+    defaults: {
+      loadingIndex: 0
+    },
+    initialize: function (pAttributes, pOptions) {
+      BurdaInfinite.models.base.BaseModel.prototype.initialize.call(this, pAttributes, pOptions);
+      this.set('list', []);
+    },
+    create: function (pData) {
+      console.log("%codoscopeArticleModel | create", "color: blue; font-weight: bold;", pData, this);
+      this.set('list', pData);
+      this.set('restoredList', _.clone(pData, true));
+
+      this.trigger('set:articleModel', this);
+    },
+    getNextURL: function () {
+      var tmpURL,
+        tmpModel;
+
+      /**
+       * Check if an Element in Array exists
+       */
+      if (!this.has('list') || this.get('list').length == 0) {
+        return null;
+      }
+
+      /**
+       * Set latest Model
+       */
+      tmpModel = this.get('list').shift();
+
+      /**
+       * Check if properties available
+       */
+      if (!tmpModel.hasOwnProperty('variantInfo') && !tmpModel.variantInfo.hasOwnProperty('ProductURL')) {
+        return null;
+      }
+
+      this.set('loadingIndex', this.get('loadingIndex') + 1);
+      tmpURL = '/lazyloading/node/' + tmpModel.variantID + '/nojs?page=' + this.get('loadingIndex');
+      console.log("%codoscopeArticleModel | getNextURL", "color: blue; font-weight: bold;", tmpURL, this);
+      return tmpURL;
+    }
+  });
+
+})(jQuery, Drupal, drupalSettings, Backbone, BurdaInfinite);
