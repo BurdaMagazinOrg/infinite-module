1b2aadb558ab338dfa5de2e0b725c935e720dab5
Steven Schulz <steven@stevenschulz.de>
2017-10-05 15:31:34 +0200

    INREL-1445 basic module setup

---
+++ modules/infinite_imagepin/src/Plugin/imagepin/Widget/ProductWidget.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\imagepin\Plugin\imagepin\Widget;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\imagepin\Plugin\WidgetBase;
+
+/**
+ * The text widget plugin.
+ *
+ * @Product(
+ *   id = "product",
+ *   label = @Translation("Text"),
+ * )
+ */
+class ProductWidget extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formNewElement(array &$form, FormStateInterface $form_state) {
+    $element = [];
+
+    // TODO Required fields currently don't work.
+    // Form API documentation lacks here, again.
+    $element['product'] = [
+      '#type' => 'textfield',
+      '#title' => t('Text').'ccsdf',
+      '#required' => FALSE,
+      '#weight' => 10,
+    ];
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function previewContent($value) {
+    return ['#markup' => '<p>' . $value['text'] . '</p>'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewContent($value) {
+    return ['#markup' => '<p>' . $value['text'] . '</p>'];
+  }
+
+}
