82389e65a87f91c9b3ae44f02b03d85ec9ef6bc5
Steven Schulz <steven@stevenschulz.de>
2017-10-11 08:48:43 +0000

    feature/INREL-1445 submodule for imagepin

---
+++ modules/infinite_imagepin/src/Plugin/imagepin/Widget/ProductWidget.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\imagepin\Plugin\imagepin\Widget;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\imagepin\Plugin\WidgetBase;
+
+/**
+ * The text widget plugin.
+ *
+ * @Widget(
+ *   id = "product",
+ *   label = @Translation("Product"),
+ * )
+ */
+class ProductWidget extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formNewElement(array &$form, FormStateInterface $form_state) {
+    $element = [];
+
+    // TODO Required fields currently don't work.
+    // Form API documentation lacks here, again.
+    $element['product'] = [
+        '#type' => 'entity_autocomplete',
+        '#target_type' => 'advertising_product',
+        '#title' => t('Product'),
+        '#required' => FALSE,
+        '#weight' => 10,
+    ];
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function previewContent($value) {
+    return ['#markup' => '<p>xxx' . $value['product'] . '</p>'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewContent($value) {
+    return ['#markup' => '<p>xxcd' . $value['product'] . '</p>'];
+  }
+
+}
