2b330c754eb2c4e6efdeb353624e8d5a74c34dd9
Steven Schulz <steven@stevenschulz.de>
2017-10-19 14:24:59 +0000

    feature/INREL-1445 load pins directly

--- modules/infinite_imagepin/src/Plugin/imagepin/Widget/ProductWidget.php
+++ modules/infinite_imagepin/src/Plugin/imagepin/Widget/ProductWidget.php
@@ -16,38 +16,47 @@ use Drupal\imagepin\Plugin\WidgetBase;
  */
 class ProductWidget extends WidgetBase {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function formNewElement(array &$form, FormStateInterface $form_state) {
-    $element = [];
+    /**
+     * {@inheritdoc}
+     */
+    public function formNewElement(array &$form, FormStateInterface $form_state) {
+        $element = [];
 
-    // TODO Required fields currently don't work.
-    // Form API documentation lacks here, again.
-    $element['product'] = [
-      '#type' => 'entity_autocomplete',
-      '#target_type' => 'advertising_product',
-      '#title' => t('Product'),
-      '#required' => FALSE,
-      '#weight' => 10,
+        // TODO Required fields currently don't work.
+        // Form API documentation lacks here, again.
+        $element['product'] = [
+            '#type' => 'entity_autocomplete',
+            '#target_type' => 'advertising_product',
+            '#title' => t('Product'),
+            '#required' => FALSE,
+            '#weight' => 10,
 
-    ];
+        ];
 
-    return $element;
-  }
+        return $element;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function previewContent($value) {
-    return ['#markup' => $value['product']];
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function previewContent($value) {
+        return ['#markup' => $value['product']];
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function viewContent($value) {
-     return ['#markup' => $value['product']];
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function viewContent($value) {
+        $entity_type = 'advertising_product';
+        $entity_id = $value['product']; // static for example purpose
+        $view_mode = 'amp';
+
+        $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($entity_id);
+        $view_builder = \Drupal::entityTypeManager()->getViewBuilder('advertising_product');
+        $pre_render = $view_builder->view($entity, $view_mode);
+        $render_output = render($pre_render);
+
+        return ['#markup' => $render_output ];
+    }
 
 }
