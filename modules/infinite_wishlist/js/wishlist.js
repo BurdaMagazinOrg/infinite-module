e15454ea1e123fa2f5e5d75fd4f5d9f3440b729f
Markus Goedecke <markus.goedecke@one-pixel-ahead.com>
2017-10-20 17:06:21 +0200

    INREL-1854 :: INREL-2970 Save product id in local storage

--- modules/infinite_wishlist/js/wishlist.js
+++ modules/infinite_wishlist/js/wishlist.js
@@ -1,4 +1,34 @@
 Drupal.behaviors.infiniteWishlist = {
+    getWishlist: function () {
+        var wishlist = localStorage.getItem('infinite_wishlist');
+        if (null === wishlist) {
+            return [];
+        } else {
+            return JSON.parse(wishlist);
+        }
+    },
+
+    storeItem: function (productId) {
+        var wishlist = this.getWishlist();
+
+        var alreadyPresent = false;
+        for (var i = 0; i < wishlist.length; i++) {
+            if (productId === wishlist[i].productId) {
+                alreadyPresent = true;
+                break;
+            }
+        }
+        if (alreadyPresent) {
+            this.growl('item ' + productId + ' is already present in wishlist');
+            return;
+        }
+        wishlist.push({
+            productId: productId
+        });
+        localStorage.setItem('infinite_wishlist', JSON.stringify(wishlist));
+        this.growl('added item ' + productId + ' to wishlist');
+    },
+
     injectIcons: function () {
         var items = document.getElementsByClassName('item-ecommerce');
         for (var i = 0; i < items.length; i++) {
@@ -8,9 +38,9 @@ Drupal.behaviors.infiniteWishlist = {
             icon.innerText = '❤';
             icon.className = 'wishlist__icon--add';
             icon.productId = item.getAttribute('data-product-id');
-            icon.addEventListener('click', function (e)  {
+            icon.addEventListener('click', function (e) {
                 e.stopPropagation();
-                Drupal.behaviors.infiniteWishlist.growl('added item ' + e.currentTarget.productId + ' to wishlist');
+                Drupal.behaviors.infiniteWishlist.storeItem(e.currentTarget.productId);
             });
 
             item.insertBefore(icon, item.firstChild);
@@ -28,38 +58,42 @@ Drupal.behaviors.infiniteWishlist = {
     },
 
     attach: function () {
-        this.injectIcons();
+        try {
+            var test = 'test';
+            localStorage.setItem(test, test);
+            localStorage.removeItem(test);
+            this.injectIcons();
+        } catch (e) { // local storage is unavailable
+            return false;
+        }
     }
 };
 
 
-
-
-
-    // var container = document.createElement('div');
-    // container.id = 'wishlist__container';
-    //
-    // var button = document.createElement('button');
-    // button.innerText = 'add to wishlist';
-    // button.setAttribute('style', 'color: darkred; background: black;');
-    // button.addEventListener('click', function() {
-    //
-    // });
-    //
-    // var list = document.createElement('ul');
-    // list.id = 'wishlist';
-    //
-    // document.body.insertBefore(container, document.body.firstChild);
-    // container.appendChild(button);
-    // container.appendChild(list);
-    //
-    // var items = [{
-    //     'title': 'Hello',
-    //     'url': 'http://instyle.local/test',
-    // }];
-    // for(var i = 0; i < items.length; i++) {
-    //     var item = items[i];
-    //     var li = document.createElement('li');
-    //     li.innerHTML = '<a href="' + item.url + '">' + item.title + '</a>';
-    //     list.appendChild(li);
-    // }
+// var container = document.createElement('div');
+// container.id = 'wishlist__container';
+//
+// var button = document.createElement('button');
+// button.innerText = 'add to wishlist';
+// button.setAttribute('style', 'color: darkred; background: black;');
+// button.addEventListener('click', function() {
+//
+// });
+//
+// var list = document.createElement('ul');
+// list.id = 'wishlist';
+//
+// document.body.insertBefore(container, document.body.firstChild);
+// container.appendChild(button);
+// container.appendChild(list);
+//
+// var items = [{
+//     'title': 'Hello',
+//     'url': 'http://instyle.local/test',
+// }];
+// for(var i = 0; i < items.length; i++) {
+//     var item = items[i];
+//     var li = document.createElement('li');
+//     li.innerHTML = '<a href="' + item.url + '">' + item.title + '</a>';
+//     list.appendChild(li);
+// }
