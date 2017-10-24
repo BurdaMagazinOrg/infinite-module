73f65ca9389168e2d7c0b926c4d6d0d6cba226b8
Steven Schulz <steven@stevenschulz.de>
2017-10-24 12:29:07 +0000

    feature-INREL-1445 remove lib and add custom lib

---
+++ modules/infinite_imagepin/js/infinite_imagepin.view.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * JS implementation for viewing imagepin items.
+ */
+
+(function ($, Drupal, drupalSettings, window) {
+
+  'use strict';
+
+  Drupal.imagepin = Drupal.imagepin || {};
+
+    alert("Your Clicked on the button");
+
+    Drupal.imagepin.onDesktop = function (widgets) {
+    return false;
+  };
+
+
+}(jQuery, Drupal, drupalSettings, window));
