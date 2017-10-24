afb55e74f81a71477b902f07f3d6f1d36120fba8
Markus Goedecke <markus.goedecke@one-pixel-ahead.com>
2017-10-24 18:06:52 +0200

    update wishlist on focus

--- modules/infinite_wishlist/js/wishlist-page.js
+++ modules/infinite_wishlist/js/wishlist-page.js
@@ -1,14 +1,24 @@
 Drupal.behaviors.infiniteWishlistPage = {
+    render: function (storedWishlist) {
+        var container = document.getElementById('wishlist-page');
+        container.innerHTML = '';
+        for(var i = 0; i < storedWishlist.length; i++) {
+            var item = storedWishlist[i];
+            var li = document.createElement('li');
+            li.innerHTML = item.markup;
+            container.appendChild(li);
+        }
+    },
+
     attach: function () {
         var container = document.getElementById('wishlist-page');
         if (container) {
-            Drupal.behaviors.infiniteWishlist.fetchProducts(function (storedWishlist) {
-                for(var i = 0; i < storedWishlist.length; i++) {
-                    var item = storedWishlist[i];
-                    var li = document.createElement('li');
-                    li.innerHTML = item.markup;
-                    container.appendChild(li);
-                }
+            Drupal.behaviors.infiniteWishlist.fetchProducts(this.render);
+
+            window.addEventListener('focus', function () {
+                Drupal.behaviors.infiniteWishlist.fetchProducts(
+                    Drupal.behaviors.infiniteWishlistPage.render
+                );
             });
         }
     }
