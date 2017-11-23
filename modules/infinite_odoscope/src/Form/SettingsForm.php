f0e6d0c046a54f2a2f5d2ce4cfd8ad07e5fd8980
Sergej Burbach <s.burbach@gmx.net>
2017-11-23 16:55:19 +0100

    cache odoscope library locally

--- modules/infinite_odoscope/src/Form/SettingsForm.php
+++ modules/infinite_odoscope/src/Form/SettingsForm.php
@@ -7,9 +7,16 @@
 
 namespace Drupal\infinite_odoscope\Form;
 
+use Drupal;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use function drupal_flush_all_caches;
+use function drupal_set_message;
+use Exception;
+use function file_put_contents;
+use GuzzleHttp\Exception\ClientException;
+use function implode;
+use function infinite_odoscope_update_library_source;
 
 /**
  * Class SettingsForm.
@@ -61,6 +68,29 @@ class SettingsForm extends ConfigFormBase {
       '#maxlength' => 128,
       '#default_value' => $config->get('odoscope_url'),
     );
+
+    $library_source = Drupal::config('infinite_odoscope.settings')->get('library_source');
+    $library_source_description = [
+      'The source of the odoscope js library.',
+      'This value is normally set in settings.php and will not be stored.',
+      'You can override it here for one update.',
+      'To download the new library source you have to check the "Update odoscope library" checkbox below.'
+    ];
+
+    $form['library_source'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Odoscope library source'),
+      '#description' => $this->t(implode(' ', $library_source_description)),
+      '#default_value' => isset($library_source) ? $library_source : $config->get('library_source'),
+    );
+
+    $form['library_update'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Update odoscope library'),
+      '#description' => $this->t('Download a new version of the odoscope library.'),
+      '#default_value' => FALSE,
+    );
+
     $enabled = $config->get('odoscope_enabled');
     $form['odoscope_enabled'] = array(
       '#type' => 'checkbox',
@@ -77,6 +107,39 @@ class SettingsForm extends ConfigFormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
+
+    if($form_state->getValue('library_update')) {
+      $library_source = $form_state->getValue('library_source');
+      $client = Drupal::httpClient();
+      try {
+        $response = $client->get($library_source);
+
+        if($response->getStatusCode() == 200) {
+          $response_body = $response->getBody()->getContents();
+          $form_state->setValue('library_source', $response_body);
+        }
+        else {
+          $form_state->setError($form['library_source'], $this->t('The odoscope library could not be updated. Response status code: %code for URL %url', [
+            '%code' => $response->getStatusCode(),
+            '%url' => $library_source
+          ]));
+        }
+
+      }
+      catch (ClientException $e) {
+        $response = $e->getResponse();
+
+        $form_state->setError($form['library_source'], $this->t('The odoscope library could not be updated. Response status code: %code for URL %url', [
+          '%code' => $response->getStatusCode(),
+          '%url' => $library_source,
+        ]));
+      }
+      catch (Exception $e) {
+        $form_state->setError($form['library_source'], $this->t('The odoscope library could not be updated. %message', [
+          '%message' => $e->getMessage(),
+        ]));
+      }
+    }
   }
 
   /**
@@ -96,6 +159,15 @@ class SettingsForm extends ConfigFormBase {
     $this->config('infinite_odoscope.settings')
       ->set('odoscope_url', $form_state->getValue('odoscope_url'))
       ->save();
+
+    $this->config('infinite_odoscope.settings')
+      ->set('library_source', $form_state->getValue('library_source'))
+      ->save();
+
+    if($form_state->getValue('library_update')) {
+      infinite_odoscope_update_library_source($form_state->getValue('library_source'), 'public://odoscope/odoscope.main.js');
+      drupal_set_message($this->t('The odoscope library was updated successfully.'));
+    }
 
     if($this->config('infinite_odoscope.settings')
       ->get('odoscope_enabled') != $form_state->getValue('odoscope_enabled')){
