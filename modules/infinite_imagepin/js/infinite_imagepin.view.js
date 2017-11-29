a8370ed4137b86dce5d38a6cc93a98abc6edb79c
Markus Goedecke <markus.goedecke@one-pixel-ahead.com>
2017-11-29 16:41:42 +0100

    fix duplicate elements on imagepin INREL-3154

--- modules/infinite_imagepin/js/infinite_imagepin.view.js
+++ modules/infinite_imagepin/js/infinite_imagepin.view.js
@@ -103,7 +103,7 @@
     pin.before(overlay);
     pin.trigger('overlay:show', [overlay]);
 
-    $widget_content = overlay.children().wrapAll('<div class="imagepin-widget-content"></div>');
+    // $widget_content = overlay.children().wrapAll('<div class="imagepin-widget-content"></div>');
     $arrow = $('<span class="arrow"></span>').appendTo(overlay.find('.imagepin-widget-content'));
 
     overlay.attr('class', widget.attr('data-imagepin-overlay-class'));
