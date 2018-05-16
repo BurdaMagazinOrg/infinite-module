3ca3467ff80deadec3791c53f289cbfd29ba0529
Steffen Schlaer <steffen.schlaer@it-cru.de>
2018-05-16 18:11:24 +0200

    INREL-3741: Optimize generation of REWE button compatible json ld

--- modules/infinite_recipe/src/SchemaMetatagManager.php
+++ modules/infinite_recipe/src/SchemaMetatagManager.php
@@ -8,6 +8,7 @@ namespace Drupal\infinite_recipe;
  * @package Drupal\schema_metatag
  */
 class SchemaMetatagManager extends \Drupal\schema_metatag\SchemaMetatagManager {
+
   /**
    * {@inheritdoc}
    */
@@ -17,25 +18,31 @@ class SchemaMetatagManager extends \Drupal\schema_metatag\SchemaMetatagManager {
     // The current implementation of REWE button does not work
     // with the graph structure. Therefore, we look for recipeIngredient as
     // our recipe indicator and then copy all items from graph so the REWE button
-    // can find them
-    if (isset($items['@graph'][0]['recipeIngredient'])) {
-      foreach ($items['@graph'][0] as $key => $graphItem) {
-        $items[$key] = $graphItem;
-      }
-      // cleanup the ingredients (strip all whitespace etc.)
-      foreach ($items['recipeIngredient'] as &$ingredient) {
-        $ingredient = preg_replace('#[\s\s]+#', ' ', $ingredient);
-        $ingredient = str_replace('&nbsp;', ' ', $ingredient);
-        $ingredient = trim($ingredient, " \t\n\r\0\x0B\xC2\xA0");
-      }
-      // The REWE button can't handle the image information as array,
-      // therefore we have to replace the array with a string containing
-      // only the image url
-      if (isset($items['image']) && is_array($items['image'])) {
-        $items['image'] = $items['image']['url'];
+    // can find them.
+    foreach ($items['@graph'] as $key => $item) {
+      if ($item['@type'] == 'Recipe') {
+
+        // Copy all recipe items out of graph structure.
+        foreach ($items['@graph'][$key] as $graphKey => $graphItem) {
+          $items[$graphKey] = $graphItem;
+        }
+
+        // cleanup the ingredients (strip all whitespace etc.)
+        foreach ($items['recipeIngredient'] as &$ingredient) {
+          $ingredient = preg_replace('#[\s\s]+#', ' ', $ingredient);
+          $ingredient = str_replace('&nbsp;', ' ', $ingredient);
+          $ingredient = trim($ingredient, " \t\n\r\0\x0B\xC2\xA0");
+        }
+
+        // The REWE button can't handle the image information as array,
+        // therefore we have to replace the array with a string containing
+        // only the image url
+        if (isset($items['image']) && is_array($items['image'])) {
+          $items['image'] = $items['image']['url'];
+        }
       }
     }
-
     return $items;
   }
+
 }
