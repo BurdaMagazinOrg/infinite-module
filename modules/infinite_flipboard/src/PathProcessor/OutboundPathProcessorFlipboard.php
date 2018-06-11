1590584326d5f33f9fd1372f29f676e2dcb28093
Gerhard Killesreiter <gerhard@tag1consulting.com>
2018-06-11 19:20:30 +0200

    INREL-3569: fix error in outbound processor

--- modules/infinite_flipboard/src/PathProcessor/OutboundPathProcessorFlipboard.php
+++ modules/infinite_flipboard/src/PathProcessor/OutboundPathProcessorFlipboard.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 class OutboundPathProcessorFlipboard implements OutboundPathProcessorInterface {
 
   function processOutbound($path, &$options = array(), Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL) {
-    if (strpos($path, '/flipboard.xml') !== FALSE && strpos($path, '/taxonomy/term/') === TRUE) {
+    if (strpos($path, '/flipboard.xml') !== FALSE && strpos($path, '/taxonomy/term/') === 0) {
       $path_args = explode("/", ltrim($path, "/"));
 
       if (is_numeric($path_args[2])) {
