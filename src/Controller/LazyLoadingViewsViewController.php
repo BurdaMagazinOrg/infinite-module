fb5e3c2a8d67d3add0893003781d7fae5e4d3f70
Markus Goedecke <markus.goedecke@one-pixel-ahead.com>
2019-03-01 09:27:17 +0100

    add route to render infinite_taxonomy_term view from ajax call

---
+++ src/Controller/LazyLoadingViewsViewController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\infinite_base\Controller;
+
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\InsertCommand;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\views\Views;
+
+/**
+ * Defines a controller to render a single node.
+ */
+class LazyLoadingViewsViewController extends ControllerBase {
+
+  public function ajaxView(Term $channel, $page) {
+    if (FALSE === is_numeric($page)) {
+      throw new \Exception('Page number needs to be numeric');
+    }
+
+    $args = [$channel->id()];
+    $view = Views::getView('infinite_taxonomy_term');
+    if (is_object($view)) {
+      $view->setArguments($args);
+      $display = 'show_more_stream';
+      $view->setDisplay($display);
+      $view->setCurrentPage($page);
+      // need to overwrite offset here, because it can't be changed
+      // separately per display
+      $view->setOffset(0);
+
+      $view->execute($display);
+      $preview = $view->preview($display);
+      $content = render($preview);
+    }
+    else {
+      throw new \Exception('view not found');
+    }
+
+    $options = [];
+    $response = new AjaxResponse();
+    $response->addCommand(new InsertCommand('', $content, $options));
+    return $response;
+  }
+}
