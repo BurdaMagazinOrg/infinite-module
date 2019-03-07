c8049277e8a0fc4e8dedf8c498656136133e37af
Markus Goedecke <markus.goedecke@one-pixel-ahead.com>
2019-03-07 15:38:43 +0100

    Revert "INREL-5437 load teaser stream instead of next article"
    This reverts commit e39be3f220eef314ec1cbea2b3b22ba9780682ba.

--- src/Plugin/Block/InfiniteLazyLoadingSimple.php
+++ src/Plugin/Block/InfiniteLazyLoadingSimple.php
@@ -22,48 +22,42 @@ class InfiniteLazyLoadingSimple extends BlockBase {
     $cache = ['contexts' => ['url.path']];
 
     if ($node = \Drupal::request()->attributes->get('node')) {
-      if (true) {
-        $lazy_loading_url = '/lazyloading-test/724/0';
-        $title = '';
-        $next_nid = null;
-      } else {
-        /* @var Node $node */
-        $query = \Drupal::entityQuery('node')
-          ->condition('created', $node->getCreatedTime(), '<')
-          ->condition('type', 'article')
-          ->condition('status', 1)
-          ->condition('promote', 1)
-          ->condition('nid', $node->id(), '!=')
-          ->sort('created', 'DESC')
-          ->range(0, 1);
+      /* @var Node $node */
+      $query = \Drupal::entityQuery('node')
+        ->condition('created', $node->getCreatedTime(), '<')
+        ->condition('type', 'article')
+        ->condition('status', 1)
+        ->condition('promote', 1)
+        ->condition('nid', $node->id(), '!=')
+        ->sort('created', 'DESC')
+        ->range(0, 1);
 
-        // Filter content promoted to front or channel page.
-        // todo: add field / value exists checks.
-        //  $promoteOptions = $query->orConditionGroup()
-        //    ->condition('promote', 1)
-        //    ->condition('promote_channel', 1);
-        //  $group = $query->andConditionGroup()
-        //    ->condition($promoteOptions);
-        //  $query->condition($group);
-        //
-        // Perhaps we could use above code later again.
+      // Filter content promoted to front or channel page.
+      // todo: add field / value exists checks.
+      //  $promoteOptions = $query->orConditionGroup()
+      //    ->condition('promote', 1)
+      //    ->condition('promote_channel', 1);
+      //  $group = $query->andConditionGroup()
+      //    ->condition($promoteOptions);
+      //  $query->condition($group);
+      //
+      // Perhaps we could use above code later again.
 
-        $next_nid = $query->execute();
-        $title = '';
+      $next_nid = $query->execute();
+      $title = '';
 
-        if (!empty($next_nid)) {
-          $page = 1;
-          if (\Drupal::request()->query->has('page')) {
-            $page = \Drupal::request()->query->getInt('page') + 1;
-          }
-
-          $next_nid = array_shift($next_nid);
-          $lazy_loading_url = '/lazyloading/node/' . $next_nid . '/nojs?page=' . $page;
-          $next_node = Node::load($next_nid);
-          $title = $next_node->getTitle();
-        } else {
-          $lazy_loading_url = '/home?page=0';
+      if (!empty($next_nid)) {
+        $page = 1;
+        if (\Drupal::request()->query->has('page')) {
+          $page = \Drupal::request()->query->getInt('page') + 1;
         }
+
+        $next_nid = array_shift($next_nid);
+        $lazy_loading_url = '/lazyloading/node/' . $next_nid . '/nojs?page=' . $page;
+        $next_node = Node::load($next_nid);
+        $title = $next_node->getTitle();
+      } else {
+        $lazy_loading_url = '/home?page=0';
       }
 
       return array(
