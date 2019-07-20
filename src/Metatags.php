854ef86ae2613b9122729214c1ce75edd77f8a4e
Markus Goedecke <markus.goedecke@one-pixel-ahead.com>
2019-07-20 13:12:32 +0200

    refactoring set googlebot news metatag when node is ecommerce landingpage

--- src/Metatags.php
+++ src/Metatags.php
@@ -5,19 +5,65 @@ namespace Drupal\infinite_base;
 use Drupal\node\Entity\Node;
 
 class Metatags {
-  public static function injectGooglebotNews(Node $node) {
+  public function injectGooglebotNews(Node $node) {
     if (false === $node->hasField('field_meta_tags')) {
       return $node;
     }
 
-    $metaTagsValue = unserialize($node->get('field_meta_tags')
-      ->first()
-      ->getValue()['value']);
-    if (in_array($node->get('field_sponsor_type')->first()->value, ['native', 'advertorial'])) {
-      $metaTagsValue['metatag_googlebot_news'] = 'noindex, nofollow';
-      $node->set('field_meta_tags', serialize($metaTagsValue));
+    // If node is native or advertorila set googlebot news value
+    $metaTagsField = $node->get('field_meta_tags')->first();
+    if ($metaTagsField) {
+      $metaTagsValue = unserialize($metaTagsField->getValue()['value']);
+
+      if (
+        $node->hasField('field_meta_tags') &&
+        $node->get('field_sponsor_type')->first() &&
+        in_array($node->get('field_sponsor_type')->first()->value, ['native', 'advertorial'])
+      ) {
+        $this->setGooglebotNewsMetatagValue($node, $metaTagsValue);
+      }
+    } else {
+      $metaTagsValue = [];
+    }
+
+    // If node is ecommerce landing page set googlebot news value
+    if($this->nodeIsEcommerceLandingPage($node)) {
+      $this->setGooglebotNewsMetatagValue($node, $metaTagsValue);
     }
 
     return $node;
+  }
+
+  protected function nodeIsEcommerceLandingPage(Node $node) : bool {
+    if($node->hasField('field_promote_states')) {
+      $promoteStatesField = $node->get('field_promote_states')->first();
+      if ($promoteStatesField) {
+        $values = $promoteStatesField->getValue();
+        if (is_array($values)) {
+          return in_array('landing', $values);
+        }
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * @param \Drupal\node\Entity\Node $node
+   * @param array $metaTagsValue
+   * @param string $defaultValue
+   */
+  private function setGooglebotNewsMetatagValue(
+    Node $node,
+    array $metaTagsValue,
+    string $defaultValue = 'noindex, nofollow'
+  ): void {
+    // if another value is already set, do not overwrite
+    if (false === empty($metaTagsValue['metatag_googlebot_news'])) {
+      return;
+    }
+
+    $metaTagsValue['metatag_googlebot_news'] = $defaultValue;
+    $node->set('field_meta_tags', serialize($metaTagsValue));
   }
 }
